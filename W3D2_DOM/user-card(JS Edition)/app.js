1e2f241d1f6efb773ab21eb457325804e77d5a40
Alaa <aladdinabidi@gmail.com>
2023-03-10 11:19:26 +0100

    #W3D2 add dom selectors and event handlers code

---
+++ W3D2_DOM/user-card(JS Edition)/app.js
@@ -0,0 +1,15 @@
+// var username = document.getElementsByTagName("h1")
+
+function changeBg(elem){
+   elem.innerText = "click me!!"
+}
+
+
+function zoom(){
+    document.getElementsByTagName("p")[2].style.fontSize = "36px"
+}
+
+function out(){
+    document.getElementsByTagName("p")[2].style.fontSize = "16px"
+}
+
